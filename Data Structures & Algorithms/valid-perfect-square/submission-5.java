a8417d8052caecc11d47294d04af3fc1f531feba
kritik kandwal <149930398+kritikkandwal@users.noreply.github.com>
2026-04-01 14:31:43 +0530

    Add: valid-perfect-square - submission-5

---
+++ Data Structures & Algorithms/valid-perfect-square/submission-5.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num==1){
+            return true;
+        }
+        for(int i=num/2;i>0;i--){
+            if(i*i==num){
+                return true;
+            }
+        }
+        return false;
+    }
+}

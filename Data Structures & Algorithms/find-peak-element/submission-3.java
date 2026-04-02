b60fd5005798f80c5ba9195bfb22df24657a9740
kritik kandwal <149930398+kritikkandwal@users.noreply.github.com>
2026-04-02 10:30:36 +0530

    Add: find-peak-element - submission-3

---
+++ Data Structures & Algorithms/find-peak-element/submission-3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int indx=0;
+        int n=nums.length;
+        if (n == 1 || nums[0] > nums[1]) {
+            return 0;
+        }
+        for(int i=1;i<nums.length-1;i++){
+            if(nums[i-1]<nums[i] && nums[i]>nums[i+1]){
+                return i;
+            }
+        }
+        return nums.length-1;
+    }
+}
